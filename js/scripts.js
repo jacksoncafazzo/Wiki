a90ebbfd89061a5b4e7e191f85c006d84a261393
Blair Peterson and Matthew Newell <student@epicodus.com>
2016-01-14 10:05:22 -0800

    add markdown,blockElements,index page and header

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,1 @@
-$(document).ready(function() {
-    var flavors = ["cookie dough","mint","chocolate chip", "strawberry"]
-
-    flavors.forEach(function(flavor) {
-      $("#flavors").append("<li>" + flavor + "</li>")
-    });
-
-/*
-    var total = 0;
-    numbers = [1, 2 ,3 ,4];
-
-    numbers.forEach(function(number) {
-      $("#flavors").text(total += number);
-    })
-    */
-});
+$(document).ready(function() {}
